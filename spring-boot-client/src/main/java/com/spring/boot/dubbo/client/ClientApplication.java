5199a2f9facf4b4b7f5a42e23af2c9d7ce0838b8
hu.rl <hu.rl@sand.com.cn>
2018-09-03 17:38:37 +0800

    简化版dubbo

--- spring-boot-client/src/main/java/com/spring/boot/dubbo/client/ClientApplication.java
+++ spring-boot-client/src/main/java/com/spring/boot/dubbo/client/ClientApplication.java
@@ -11,14 +11,11 @@ import org.springframework.context.annotation.Bean;
  * <p>
  * Created by bysocket on 16/4/26.
  */
-// Spring Boot 应用的标识
 @SpringBootApplication
 public class ClientApplication {
 
     public static void main(String[] args) {
-        // 程序启动入口
-        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
-        ConfigurableApplicationContext run = SpringApplication.run(ClientApplication.class, args);
+        SpringApplication.run(ClientApplication.class, args);
     }
 
     @Bean
