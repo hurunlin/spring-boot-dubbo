462635441362682fc498e08ce0344994ac71bca5
hu.rl <hu.rl@sand.com.cn>
2018-09-03 17:35:55 +0800

    简化版dubbo

--- spring-boot-client/src/main/java/com/spring/boot/dubbo/client/ClientApplication.java
+++ spring-boot-client/src/main/java/com/spring/boot/dubbo/client/ClientApplication.java
@@ -1,8 +1,10 @@
 package com.spring.boot.dubbo.client;
 
+import com.alibaba.dubbo.config.ConsumerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Spring Boot 应用启动类
@@ -17,5 +19,13 @@ public class ClientApplication {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         ConfigurableApplicationContext run = SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Bean
+    public ConsumerConfig consumerConfig() {
+        ConsumerConfig consumerConfig = new ConsumerConfig();
+        consumerConfig.setCheck(false);
+        consumerConfig.setTimeout(20000);
+        return consumerConfig;
     }
 }
